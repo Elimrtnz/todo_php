0bb18bbce9c7ae67e0de019f182256a11f086b29
Elimrtnz <elimartineztwenty5@gmail.com>
2017-08-03 11:25:56 -0700

    Delete mysqlconnect.php

--- mysqlconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost','root','root','php_practice');
-
-if(!$conn){
-    echo "Error!!!".mysqli_connect_errno();
-}
-//echo "Success: A proper connection to MySQL was made!";
-
-?>
